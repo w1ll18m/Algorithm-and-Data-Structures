17546844ca09a3dcebeea8f93c681413ea619b92
w1ll18m <william.liu2568@gmail.com>
2024-09-01 16:16:54 -0400

    2487 Remove Nodes From Linked List O(1) Space

--- Linked_List/2487_Remove_Nodes_From_Linked_List.py
+++ Linked_List/2487_Remove_Nodes_From_Linked_List.py
@@ -7,52 +7,41 @@ class ListNode:
         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dec_list = []
-        cur = head
+        # reverse the linked list
+        prev, cur = None, head
 
-        # iterate through the list and create a monotonically decreasing stack
         while cur is not None:
-            while True:
-                # case when current node is less/equal to top of stack (or stack is empty)
-                if len(dec_list) == 0 or cur.val <= dec_list[-1].val:
-                    dec_list.append(cur)
-                    break
-                # case when current node is greater than top of stack
-                else:
-                    dec_list.pop(-1)
-            cur = cur.next
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
         
-        # reconstruct new list by popping from the stack (where top of stack is end of list)
-        while len(dec_list) != 0:
-            prev = dec_list.pop(-1)
-            prev.next = cur
-            cur = prev
+        # iterate through the reversed list and construct the modified list
+        last_valid = prev
+        cur = last_valid.next
+        last_valid.next = None
+
+        while cur is not None:
+            if cur.val >= last_valid.val:
+                next = cur.next
+                cur.next = last_valid
+                last_valid = cur
+                cur = next
+            else:
+                cur = cur.next
         
-        return cur
-    
+        return last_valid
+
     '''
-    remove every node which has a node with a greater value anywhere to the right side of it
+    remove every node with a node with a greater value to its right
 
-    thus we should have a monotonically decreasing linked list as the output
+    we cant just make a monotonically decreasing list starting from the first node since it might not be the largest node
+    but observe that the last node of the list will always be in the modified linked list since it has no nodes to its right
+    thus the last node of the list will always be the smallest node in the modified linked list
 
-    iterate through the list keep track of:
-        - modified list head value
-        - modified list current value
+    reverse the linked list then iterate through it
+        - keep track of the last valid node and its value
+        - if the current node is greater than the last valid node then add it to the modified linked list
 
-    if we encounter a node that:
-        - is less than modified list current value -> keep that node and set current value to it
-        - is less than modified list head but greater than current value -> how do we go back and remove those nodes????
-            - keep a MONOTONICALLY DECREASING STACK
-                - allows us to go back to previous entries in the linked list
-        - is greater than modified list head -> set modified list head to current node (which effectively removes all nodes to left)
-    
-    5 2 1  -> dont remove any nodes
-    5 2 3  -> remove 2
-    5 2 13 -> remove 5 2 
-
-    lets revise our solution to use a monotonically decreasing stack
-    iterate through the list and construct a monotonically decreasing stack:
-        - if current node is greater than top of stack then keep popping from stack until its smaller
-        - if current node is smaller than top of stack then add it to the stack
-    reconstruct new list by popping from the stack
+    O(n) time complexity and O(1) space complexity
     '''
