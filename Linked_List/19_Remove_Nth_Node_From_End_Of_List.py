e3ac189effec936f99ed8a76897b170709d85f2c
w1ll18m <william.liu2568@gmail.com>
2024-09-16 17:41:14 -0400

    19 Remove Nth Node From End Of List

--- Linked_List/19_Remove_Nth_Node_From_End_Of_List.py
+++ Linked_List/19_Remove_Nth_Node_From_End_Of_List.py
@@ -1,36 +1,37 @@
+from typing import Optional
+
 # Definition for singly-linked list.
-class ListNode(object):
+class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev_a, a = dummy, head
+        idx_b, b = 1, head
 
-        pointer_a = head
-        pointer_b = head
-        for i in range(n):                  # create an offset of n between pointer_a and pointer_b
-            pointer_b = pointer_b.next
+        # find the node at index n
+        while idx_b != n:
+            b = b.next
+            idx_b += 1
 
-        prev = None                         # keep track of the node before pointer_a
-        while pointer_b is not None:
-            prev = pointer_a
-            pointer_a = pointer_a.next
-            pointer_b = pointer_b.next
+        # iterate b until we reach the last node, a should point at the node at index m - n + 1
+        while b.next is not None:
+            b = b.next
+            prev_a = a
+            a = a.next
         
-        if prev == None:                    # if prev == None then we are removing the first node
-            return pointer_a.next
-        else:                               # remove pointer_a which is the node after prev
-            prev.next = prev.next.next
-        return head
+        # remove the node at index m - n + 1
+        prev_a.next = a.next
         
-        '''
-        observe that the nth node from the end of the list has index sz - n + 1
-        we use 2 different pointers with an offset of n
-            - when pointer_b = n + 1 then pointer_a = 1
-            - when pointer_b = None (sz + 1) then pointer_a = sz - n + 1
-        '''
+        return dummy.next
+ 
+    '''
+    given the head, remove the nth node from the end of the list
+    let the list be 1-indexed and be length m then:
+        - the nth node from the end of the list has index m - n + 1
+        - so let b = a + n - 1 so
+            - when a = 1 then b = n
+            - when a = m - n + 1 then b = m
+    '''
